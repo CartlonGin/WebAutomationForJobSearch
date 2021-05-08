51bc80548dffe001e569b87d7f46bb48ab56c7cb
Kaanhan Okyay <65706502+kaanhangithub@users.noreply.github.com>
2021-05-07 23:15:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,64 +18,73 @@ def delete(sheet):
     # return to main function
     return
 
-pathExcel = r"C:\Users\kaanh\Desktop\job.xlsx"
-workbook = openpyxl.load_workbook(pathExcel)
-sheet = workbook.active
-delete(sheet)
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+if __name__ == '__main__':
 
-driver.get("https://www.workopolis.com/en/")
+    pathExcel = r"C:\Users\kaanh\Desktop\job.xlsx"
+    workbook = openpyxl.load_workbook(pathExcel)
+    sheet = workbook.active
+    delete(sheet)
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+    
+    driver.get("https://www.workopolis.com/en/")
 
-searchTitle = driver.find_element_by_id("keyword")
-searchTitle.send_keys("software developer")
-searchTitle.send_keys(Keys.TAB)
-
-
-searchArea = driver.find_element_by_id("location")
-searchArea.send_keys("toronto")
-searchArea.send_keys(Keys.RETURN)
-
-driver.implicitly_wait(5)
+    searchTitle = driver.find_element_by_id("keyword")
+    #job title
+    searchTitle.send_keys("software developer")
+    searchTitle.send_keys(Keys.TAB)
 
 
-r = 1
-for pageNumber in range(1, 30):
-    nextPage = driver.find_element_by_link_text(str(pageNumber)).click()
-    for i in range(20):
-        jobCard = driver.find_elements_by_class_name("JobCard")
-        jobCard[i].click()
-        driver.implicitly_wait(10)
-        exist = True
-        try:
-            jobSkills = driver.find_elements_by_class_name("nav")
+    searchArea = driver.find_element_by_id("location")
+    # job location
+    searchArea.send_keys("toronto")
+    searchArea.send_keys(Keys.RETURN)
+
+    driver.implicitly_wait(5)
+    driver.find_element_by_xpath("//a[@title='Page 2']").click()
+
+
+    r = 1
+    # first 30 pages
+    for pageNumber in range(1, 30):
+        nextPage = driver.find_element_by_link_text(str(pageNumber)).click()
+        #every page has 20 job postings
+        for i in range(20):
+            jobCard = driver.find_elements_by_class_name("JobCard")
+            jobCard[i].click()
+            driver.implicitly_wait(10)
             exist = True
+            try:
+                jobSkills = driver.find_elements_by_class_name("nav")
+                exist = True
 
-        except NoSuchElementException:
-            exist = False
-        if exist:
-            for j in range(len(jobSkills)):
-                try:
-                    if jobSkills[j].text.__contains__("Python") or jobSkills[j].text.__contains__("Java"):
-                        driver.implicitly_wait(10)
-                        jobTitle = driver.find_element_by_class_name("ViewJobHeader-title")
-                        print(jobTitle.text)
-                        company = driver.find_element_by_class_name("ViewJobHeader-company")
-                        print(company.text)
-                        salary = driver.find_element_by_class_name("Salary")
-                        print(salary.text)
-                        jobDescripton = driver.find_element_by_css_selector("html[data-css~=serp] .ViewJob-description")
-                        sheet.cell(row=r, column=1).value = jobTitle.text
-                        sheet.cell(row=r, column=2).value = company.text
-                        sheet.cell(row=r, column=3).value = salary.text
-                        sheet.cell(row=r, column=4).value = jobDescripton.text
-                        r = r+1
+            except NoSuchElementException:
+                exist = False
+            if exist:
+                for j in range(len(jobSkills)):
+                    try:
+                        #check if job posting skills section has given skills
+                        if jobSkills[j].text.__contains__("Python") or jobSkills[j].text.__contains__("Java"):
+                            driver.implicitly_wait(10)
+                            jobTitle = driver.find_element_by_class_name("ViewJobHeader-title")
+                            print(jobTitle.text)
+                            company = driver.find_element_by_class_name("ViewJobHeader-company")
+                            print(company.text)
+                            salary = driver.find_element_by_class_name("Salary")
+                            print(salary.text)
+                            jobDescripton = driver.find_element_by_css_selector("html[data-css~=serp] .ViewJob-description")
+                            #Insert scraped information about job into Excel file.
+                            sheet.cell(row=r, column=1).value = jobTitle.text
+                            sheet.cell(row=r, column=2).value = company.text
+                            sheet.cell(row=r, column=3).value = salary.text
+                            sheet.cell(row=r, column=4).value = jobDescripton.text
+                            r = r+1
 
-                except exceptions.StaleElementReferenceException:
+                    except exceptions.StaleElementReferenceException:
 
-                    pass
+                        pass
 
-workbook.save(pathExcel)
+    workbook.save(pathExcel)
 
-time.sleep(5)
-driver.quit()
+    time.sleep(5)
+    driver.quit()
